924897b3f74918713e1d86a3ada00cec2b6388dc
marzenusia <nsnjjs@com.pl>
2024-10-30 12:22:11 +0100

    rotate clockwise

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<List<Integer>> matrix = new ArrayList<>(Arrays.asList(Arrays.asList(1, 2, 3,11),
+                Arrays.asList(4, 5, 6,12),
+                Arrays.asList(7, 8, 9,13),
+                Arrays.asList(10,11,12,14)));
+        Transposition transposition = new Transposition(matrix,270);
+        System.out.println(transposition.rotationMatrixXTimes());
+//        System.out.println(transposition.matrixRotate90ClockWise(matrix));
     }
 }
--- src/main/java/org/example/Transposition.java
+++ src/main/java/org/example/Transposition.java
@@ -1,5 +1,44 @@
 package org.example;
 
-public class Transposition {
+import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@AllArgsConstructor
+public class Transposition {
+    List<List<Integer>> currentMatrix;
+    int degree;
+
+    List<List<Integer>> rotationMatrixXTimes() {
+        int timesRotation = degree / 90;
+        for (int i = 0; i < timesRotation; i++) {
+            matrixRotate90ClockWise();
+        }
+        return currentMatrix;
+    }
+
+    void matrixRotate90ClockWise() {
+
+        int rows = currentMatrix.size();
+        int columns = currentMatrix.get(0).size();
+
+        List<List<Integer>> result = IntStream.range(0, rows)
+                .mapToObj(i -> IntStream.range(0, columns)
+                        .mapToObj(j -> (Integer) null)
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                result.get(i).set(j, currentMatrix.get(j).get(i));
+            }
+        }
+        currentMatrix = result;
+
+
+    }
 }
